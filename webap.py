98f30625eaaa235a75ca7167b672f09f8266aa90
yangsq10 <67204061+yangsq10@users.noreply.github.com>
2020-10-03 11:02:39 -0400

    Update webap.py

--- webap.py
+++ webap.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import cv2
-from PIL import Image
-import numpy as np
-import time
-import os
 st.title("Snow monkey checker")
 st.write("Subscribe")
 user_input=st.text_input("Your email address")
