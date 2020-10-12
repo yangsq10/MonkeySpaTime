bc12b04e4dd508e4dce428a4bb633e83aa6d0d2e
yangsq10 <67204061+yangsq10@users.noreply.github.com>
2020-10-12 09:34:19 -0400

    Delete webap.py

--- webap.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("Snow monkey checker")
-st.write("Subscribe")
-user_input=st.text_input("Your email address")
-input=st.button("Subscribe")
-if input and user_input:
-    st.write("Successfully subscribe!")
-
